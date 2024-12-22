d98f8694a4d4a9c92b2c8423b729737f473ff38b
Bruno Desktop <carecsnay1@gmail.com>
2024-12-21 21:53:28 -0300

    Fix: second hand bug on 360 deg

--- index.js
+++ index.js
@@ -5,27 +5,35 @@ const secondsHand = document.querySelector(".hand.seconds");
 const audio = document.querySelector("#Test_Audio");
 
 window.addEventListener("load", () => {
-    audio.volume = 0.3; //audio 30% pra não assustar os outros.
+    audio.volume = 0.3; // Áudio a 30% para não assustar.
     audio.play();
 });
+
+let lastSecondPercentage = 0;
 
 const setClock = () => {
     const currentDate = new Date();
 
-    const secondsPercentage = currentDate.getSeconds() / 60;
-    const minutesPercentage = (secondsPercentage + currentDate.getMinutes()) / 60;
-    const hoursPercentage = (minutesPercentage + currentDate.getHours()) / 12;
+    const secondsPercentage = (currentDate.getSeconds() + currentDate.getMilliseconds() / 1000) / 60;
+    const minutesPercentage = (currentDate.getMinutes() + secondsPercentage) / 60;
+    const hoursPercentage = (currentDate.getHours() + minutesPercentage) / 12;
 
-    setRotation(secondsHand, secondsPercentage);
+    setRotation(secondsHand, secondsPercentage, lastSecondPercentage);
     setRotation(minutesHand, minutesPercentage);
     setRotation(hoursHand, hoursPercentage);
 
+    lastSecondPercentage = secondsPercentage;
     requestAnimationFrame(setClock);
 };
 
-const setRotation = (element, rotationPercentage) => {
+const setRotation = (element, rotationPercentage, previousRotation = 0) => {
+    if (Math.abs(rotationPercentage - previousRotation) > 0.5) {
+        element.style.transition = "none";
+    } else {
+        element.style.transition = "transform 0.1s linear";
+    }
+
     element.style.setProperty("--rotation", rotationPercentage * 360);
 };
 
 setClock();
-setInterval(setClock, 1000);
